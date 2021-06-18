56779663887872a1ad96ff608501040e95c6e300
ah9828 <amund.meling@bkk.no>
2021-06-18 16:22:12 +0200

    Fix api

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   useEffect(() => {
     (async function () {
-      const { text } = await( await fetch(`/api/HelloWorld`));
+      const { text } = await( await fetch(`/api/HelloWorld`)).json();
       setData(text);
     })();
   });
